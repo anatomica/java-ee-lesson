eed8096c6c9bbef10f910300ccec66535b2227b0
Maxim <maksfomin@gmail.com>
2020-01-18 18:32:05 +0300

    Create HW 4 step 4 (add & remove product to cart)

--- base-jsf-app/src/main/java/controller/CartController.java
+++ base-jsf-app/src/main/java/controller/CartController.java
@@ -4,15 +4,59 @@ import store.Catalog;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @ApplicationScoped
 @Named
 public class CartController {
-    Map<Integer, Catalog> cart = new TreeMap<>();
+
+    private Map<String, HashSet<String>> cart;
+    // private Map<String, String> cart;
+    private String ID;
+    private String Name;
+    private String Description;
+    private String Price;
 
     public CartController() {
+        cart = new HashMap<>();
     }
 
+    public Map<String, HashSet<String>> getCart() {
+        return cart;
+    }
+
+    public String getID() {
+        return ID;
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getDescription() {
+        return Description;
+    }
+    public String getPrice() {
+        return Price;
+    }
+
+    public void addToCart(Catalog catalog) {
+        HashSet<String> ID = cart.getOrDefault("ID", new HashSet<>());
+        ID.add(catalog.getId().toString());
+        cart.put("ID", ID);
+        HashSet<String> Name = cart.getOrDefault("Name", new HashSet<>());
+        Name.add(catalog.getName());
+        cart.put("Name", Name);
+        HashSet<String> Description = cart.getOrDefault("Description", new HashSet<>());
+        Description.add(catalog.getDescription());
+        cart.put("Description", Description);
+        HashSet<String> Price = cart.getOrDefault("Price", new HashSet<>());
+        Price.add(catalog.getPrice().toString());
+        cart.put("Price", Price);
+
+
+//        cart.put("ID", catalog.getId().toString());
+//        cart.put("Name", catalog.getName());
+//        cart.put("Description", catalog.getDescription());
+//        cart.put("Price", catalog.getPrice().toString());
+
+    }
 }
