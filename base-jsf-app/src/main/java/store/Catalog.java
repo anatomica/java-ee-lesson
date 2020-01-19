fcc5581c84bd67289020428d0b3b7e22a803ff06
Maxim <maksfomin@gmail.com>
2020-01-19 22:45:16 +0300

    Create HW 4 step 8 (add javax.validation)

--- base-jsf-app/src/main/java/store/Catalog.java
+++ base-jsf-app/src/main/java/store/Catalog.java
@@ -1,13 +1,21 @@
 package store;
 
+import javax.inject.Named;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
-import java.util.HashSet;
 
+@Named
 public class Catalog {
 
     private Long id;
+    @NotNull (message = "Поле не должно быть пустым")
+    @Size (min = 2, max = 20, message = "Поле должно содержать от 4 до 10 символов")
     private String name;
+    @NotNull (message = "Поле не должно быть пустым")
+    @Size (min = 2, max = 20, message = "Поле должно содержать от 4 до 10 символов")
     private String description;
+    @NotNull (message = "Поле не должно быть пустым")
+    @Digits (integer = 6, fraction = 2)
     private BigDecimal price;
 
     public Catalog() {
@@ -20,32 +28,16 @@ public class Catalog {
         this.price = price;
     }
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 
 }
