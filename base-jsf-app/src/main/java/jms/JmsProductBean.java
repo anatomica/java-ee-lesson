f8e4436b85a69364702a08f9f7e573d60fbcb550
Anatomica <maksfomin@gmail.com>
2020-02-14 01:37:10 +0700

    Create HW 7 step 3

--- base-jsf-app/src/main/java/jms/JmsProductBean.java
+++ base-jsf-app/src/main/java/jms/JmsProductBean.java
@@ -10,32 +10,30 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 
-/*
-Перед разворачиванием приложения необходимо создать очередь на сервере приложений
-Это делается приведенной ниже командой через консоль jboss-cli.sh
-
-jms-queue add --queue-address= jms.queue.ProductQueue --entries=java:jms/queue/productQueue,java:jboss/exported/jms/queue/productQueue
-*/
-
-//@MessageDriven(activationConfig = {
-//        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-//        @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jboss/exported/jms/queue/productQueue"),
-//        @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "action = 'create'")
-//})
-//public class JmsProductBean implements MessageListener {
+//Перед разворачиванием приложения необходимо создать очередь на сервере приложений
+//Это делается приведенной ниже командой через консоль jboss-cli.sh
 //
-//    @EJB
-//    private CatalogRepository catalogRepository;
-//
-//    @Override
-//    public void onMessage(Message message) {
-//        if (message instanceof ObjectMessage) {
-//            try {
-//                Catalog product = (Catalog) ((ObjectMessage) message).getObject();
-//                catalogRepository.insert(product);
-//            } catch (JMSException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//}
+//jms-queue add --queue-address= jms.queue.ProductQueue --entries=java:jms/queue/productQueue,java:jboss/exported/jms/queue/productQueue
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "java:/jboss/exported/jms/queue/productQueue"),
+        @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "action = 'create'")
+})
+public class JmsProductBean implements MessageListener {
+
+    @EJB
+    private CatalogRepository catalogRepository;
+
+    @Override
+    public void onMessage(Message message) {
+        if (message instanceof ObjectMessage) {
+            try {
+                Catalog product = (Catalog) ((ObjectMessage) message).getObject();
+                catalogRepository.insert(product);
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
